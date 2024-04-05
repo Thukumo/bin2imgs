100fbc9b5bb203770b6ab35c56bc58164552e9c0
つくも <92912896+Thukumo@users.noreply.github.com>
2024-04-05 18:30:30 +0900

    入力

--- Program.cs
+++ Program.cs
@@ -2,6 +2,7 @@
 using System.CommandLine;
 using System.CommandLine.Invocation;
 using System.CommandLine.IO;
+using System.ComponentModel;
 using System.IO;
 using System.Threading.Tasks;
 
@@ -9,25 +10,34 @@ namespace bin2imgs
 {
     public class Program
     {
-        public static void Main(string[] args)
+        public static int Main(string[] args)
         {
             var rootCommand = new RootCommand("Convert any file to image files");
             rootCommand.Description = "bin2imgs";
-            rootCommand.AddArgument(new Argument<string>("filename"));
-            rootCommand.AddOption(new Option<bool>(new[] { "--reverse", "-r" }, "Convert movie file to file"));
-            /*
-            rootCommand.Handler = CommandHandler.Create<string, bool>((filename, rev) =>
+            var filenamearg = new Argument<string>("file-name");
+            rootCommand.Add(filenamearg);
+            var reverseOption = new Option<bool>(new[] { "--reverse", "-r" }, "Convert movie file to file");
+            reverseOption.SetDefaultValue(false);
+            reverseOption.AddAlias("-r");
+            rootCommand.Add(reverseOption);
+            string filename = "";
+            bool rev = false;
+            rootCommand.SetHandler((fileName, reverse) =>
             {
-                reverse = rev;
-                if (!reverse && !File.Exists(filename))
-                {
-                    Console.WriteLine("file not found");
-                    return;
-                }
-            });
-            */
+                //Console.WriteLine($"filename = {fileName}");
+                //Console.WriteLine($"--reverse = {reverse}");
+                filename = fileName;
+                rev = reverse;
+            },
+            filenamearg, reverseOption);
             rootCommand.InvokeAsync(args).Wait();
-            //Console.WriteLine();
+            if(!File.Exists(filename))
+            {
+                Console.Error.WriteLine($"File not found: {filename}");
+                return(1);
+            }
+            Console.WriteLine();
+            return 0;
         }
     }
 }
