5aa2e71dba2a927cca118859d3ba18706e53955c
つくも <92912896+Thukumo@users.noreply.github.com>
2024-04-05 16:09:24 +0900

    ?

--- Program.cs
+++ Program.cs
@@ -1,1 +1,38 @@
-﻿Console.WriteLine("Hello World!");
+﻿using System;
+using System.CommandLine;
+using System.CommandLine.Invocation;
+using System.CommandLine.IO;
+using System.IO;
+using System.Threading.Tasks;
+
+namespace bin2imgs
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var rootCommand = new RootCommand
+            {
+            new Option<string>(
+                    "-f",
+                    description: "Specify input file name(filename)"),
+                new Option<bool>(
+                    "-r",
+                    getDefaultValue: () => false,
+                description: "Do mov2bin.(reverse)")
+            };
+            rootCommand.Description = "bin2imgs";
+            bool reverse = false;
+            rootCommand.Handler = CommandHandler.Create<string, bool>((filename, rev) =>
+            {
+                reverse = rev;
+                if (!reverse && !File.Exists(filename))
+                {
+                    Console.WriteLine("file not found");
+                    return;
+                }
+            });
+            rootCommand.InvokeAsync(args).Wait();
+        }
+    }
+}
